87192dfa42658d7b27dd20cc7d27a34b08b97d68
Tony <rbbtsft@gmail.com>
2014-10-22 22:32:58 -0700

    Creating initial project.

---
+++ App/Merchant/Merchant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Merchant
+//
+//  Created by User on 10/21/14.
+//  Copyright (c) 2014 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ App/Merchant/Merchant/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Merchant
+//
+//  Created by User on 10/21/14.
+//  Copyright (c) 2014 com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
