02a1af00b0f05f83efd4ada52b68c71b6d58a545
Tony <rbbtsft@gmail.com>
2014-11-13 06:44:57 -0800

    Version bump

--- App/Merchant/Merchant/BCMConstants.h
+++ App/Merchant/Merchant/BCMConstants.h
@@ -9,7 +9,7 @@
 #ifndef Merchant_BCMConstants_h
 #define Merchant_BCMConstants_h
 
-#define APP_BUILD_VERSION @"00.00.14"
+#define APP_BUILD_VERSION @"00.00.15"
 #define MERCHANT_TEST_ADDRESS @"123H9LYYaBvuf97EqbJpSYPCjDmu5C1btH"
 
 #define BC_STRING_PLEASE_ENTER_PIN NSLocalizedString(@"Please enter your PIN", nil)
