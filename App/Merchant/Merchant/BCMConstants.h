ef8fa67b580e361bd2edfd7b52aed28012271fcb
Tony <rbbtsft@gmail.com>
2014-11-13 06:44:11 -0800

    Adding PIN entry localized strings and other defines

--- App/Merchant/Merchant/BCMConstants.h
+++ App/Merchant/Merchant/BCMConstants.h
@@ -9,6 +9,25 @@
 #ifndef Merchant_BCMConstants_h
 #define Merchant_BCMConstants_h
 
-#define APP_BUILD_VERSION @"00.00.05"
+#define APP_BUILD_VERSION @"00.00.14"
+#define MERCHANT_TEST_ADDRESS @"123H9LYYaBvuf97EqbJpSYPCjDmu5C1btH"
+
+#define BC_STRING_PLEASE_ENTER_PIN NSLocalizedString(@"Please enter your PIN", nil)
+#define BC_STRING_PLEASE_ENTER_NEW_PIN NSLocalizedString(@"Please enter a new PIN", nil)
+#define BC_STRING_CONFIRM_PIN NSLocalizedString(@"Confirm your PIN", nil)
+#define BC_STRING_INCORRECT_PIN_RETRY NSLocalizedString(@"Incorrect PIN. Please retry.", nil)
+#define RETRY_VALIDATION NSLocalizedString(@"Retry Validation", nil)
+#define BC_STRING_SERVER_RETURNED_NULL_STATUS_CODE NSLocalizedString(@"Server Returned Null Status Code", nil)
+#define BC_STRING_PIN_VALIDATION_CANNOT_BE_COMPLETED NSLocalizedString(@"PIN Validation cannot be completed. Please enter your wallet password manually.", nil)
+#define BC_STRING_PIN_RESPONSE_OBJECT_SUCCESS_LENGTH_0 NSLocalizedString(@"PIN Response Object success length 0", nil)
+#define BC_STRING_DECRYPTED_PIN_PASSWORD_LENGTH_0 NSLocalizedString(@"Decrypted PIN Password length 0", nil)
+#define BC_STRING_CANNOT_SAVE_PIN_CODE_WHILE NSLocalizedString(@"Cannot save PIN Code while wallet is not initialized or password is null", nil)
+#define BC_STRING_INVALID_STATUS_CODE_RETURNED NSLocalizedString(@"Invalid Status Code Returned %@", nil)
+#define BC_STRING_PIN_RESPONSE_OBJECT_KEY_OR_VALUE_LENGTH_0 NSLocalizedString(@"PIN Response Object key or value length 0", nil)
+#define BC_STRING_PIN_ENCRYPTED_STRING_IS_NIL NSLocalizedString(@"PIN Encrypted String is nil", nil)
+#define BC_STRING_PIN_SAVED_SUCCESSFULLY NSLocalizedString(@"PIN Saved Successfully", nil)
+#define BC_STRING_CANCEL NSLocalizedString(@"Cancel", nil)
+
+//#define MOCK_BTC_TRANSACTION 1
 
 #endif
