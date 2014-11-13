cb8069ae7e1b105a5c60cf1e9c8b5336e33a309c
Tony <rbbtsft@gmail.com>
2014-11-13 06:41:54 -0800

    Including detail web view support for BTC transactions

---
+++ App/Merchant/Merchant/BCMWebViewController.m
@@ -0,0 +1,59 @@
+//
+//  BCMWebViewController.m
+//  Merchant
+//
+//  Created by User on 11/7/14.
+//  Copyright (c) 2014 com. All rights reserved.
+//
+
+#import "BCMWebViewController.h"
+
+#import "Transaction.h"
+
+@interface BCMWebViewController ()
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+@property (copy, nonatomic) NSString *transactionURLString;
+
+@end
+
+@implementation BCMWebViewController
+
+- (void)viewDidLoad {
+
+    [super viewDidLoad];
+    
+    [self addNavigationType:BCMNavigationTypeCancel position:BCMNavigationPositionLeft selector:@selector(cancelAction:)];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    NSLog(@"Loading %@", self.transactionURLString);
+    
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:self.transactionURLString]]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@synthesize transaction = _transaction;
+
+- (void)setTransaction:(Transaction *)transaction
+{
+    _transaction = transaction;
+    
+    self.transactionURLString = [NSString stringWithFormat:@"https://blockchain.info/tx/%@", transaction.transactionHash];
+}
+
+#pragma mark - Actions
+
+- (void)cancelAction:(id)sender
+{
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
