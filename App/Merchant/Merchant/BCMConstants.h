3c25af8ed5f40bf8b62f7fa03c716531168eb017
Tony <rbbtsft@gmail.com>
2014-12-02 09:47:17 -0800

    removing invalid define

--- App/Merchant/Merchant/BCMConstants.h
+++ App/Merchant/Merchant/BCMConstants.h
@@ -9,7 +9,7 @@
 #ifndef Merchant_BCMConstants_h
 #define Merchant_BCMConstants_h
 
-#define APP_BUILD_VERSION @"00.00.22"
+#define APP_BUILD_VERSION @"00.00.23"
 #define MERCHANT_TEST_ADDRESS @"123H9LYYaBvuf97EqbJpSYPCjDmu5C1btH"
 
 #define MAIN_STORYBOARD_NAME @"Main"
@@ -17,8 +17,6 @@
 #define HELVETICA_NEUE_LIGHT_15 [UIFont fontWithName:@"HelveticaNeue-Light" size:15.0f]
 
 // Localized Constants
-
-#define LOCALIZED_ALERT_OK NSLocalizedString(@"alert.OK", nil)
 
 //#define MOCK_BTC_TRANSACTION 1
 
